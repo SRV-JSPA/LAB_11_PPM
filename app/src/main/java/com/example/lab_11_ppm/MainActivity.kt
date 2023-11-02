29cc043ef531e8ce607e85cc733e57e7c9562b97
JSPA <per22318@uvg.edu.gt>
2023-11-02 17:17:51 -0600

    cambios en el app

--- app/src/main/java/com/example/lab_11_ppm/MainActivity.kt
+++ app/src/main/java/com/example/lab_11_ppm/MainActivity.kt
@@ -1,129 +1,139 @@
 package com.example.lab_11_ppm
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
-import android.os.Build
+import android.location.LocationManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
-import android.util.Log
+import android.provider.Settings
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
 import com.example.lab_11_ppm.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.Granularity
 import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    val PERMISSION_ID = 42
 
-    private val CODIGO_PERMISO_SEGUNDO_PLANO = 100
-    private var isPermisos = false
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationCallBack: LocationCallback
+
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        verificarPermisos()
-    }
-
-    private fun verificarPermisos(){
-        val permisos = arrayListOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            permisos.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        if(allPermissionsGrantedGPS()){
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            leerUbicacionActual()
+        } else{
+            ActivityCompat.requestPermissions(this,arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_ID)
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         }
 
-        val permisosArray = permisos.toTypedArray()
-        if(tienePermisos(permisosArray)){
-            isPermisos = true
-            onPermisosConcedidos()
-        } else {
-            solicitarPermisos(permisosArray)
+
+        binding.btnVer.setOnClickListener{
+            leerUbicacionActual()
         }
     }
 
-    private fun tienePermisos(permisos: Array<String>): Boolean{
-       return permisos.all{
-           return ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
-       }
+    private fun allPermissionsGrantedGPS() = Companion.REQUIRED_PERMISSIONS_GPS.all{
+        ContextCompat.checkSelfPermission(baseContext,it)== PackageManager.PERMISSION_GRANTED
     }
 
-    private fun onPermisosConcedidos(){
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        try{
-            fusedLocationClient.lastLocation.addOnSuccessListener {
-                if(it != null){
-                    imprimirUbicacion(it)
-                } else{
-                    Toast.makeText(this, "No se Puede obtener la ubicación", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            val locationRequest = LocationRequest.Builder(
-                Priority.PRIORITY_HIGH_ACCURACY,
-                30000
-            ).apply {
-                setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
-                setWaitForAccurateLocation(true)
-            }.build()
-
-            locationCallBack = object : LocationCallback(){
-                override fun onLocationResult(p0: LocationResult) {
-                    super.onLocationResult(p0)
-
-                    for(location in p0.locations){
-                        imprimirUbicacion(location)
+    private fun leerUbicacionActual(){
+        if (checkPermissions()){
+            if(isLocationEnabled()){
+                if(ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.lastLocation.addOnCompleteListener { task ->
+                        var location: Location? = task.result
+                        if(location==null){
+                            requestNewLocationData()
+                        } else{
+                            binding.tv1.text = "LATITUD = "+location.latitude.toString()
+                            binding.tv2.text = "LONGITUD = "+location.longitude.toString()
+                        }
                     }
+
                 }
+            } else{
+                Toast.makeText(this,"Activar Ubicacion",Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                this.finish()
             }
-
-            fusedLocationClient.requestLocationUpdates(
-                locationRequest,
-                locationCallBack,
-                Looper.getMainLooper()
-            )
-        } catch (_: SecurityException){
-
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),PERMISSION_ID)
         }
     }
 
-    private fun solicitarPermisos(permisos: Array<String>){
-        requestPermissions(
-            permisos,
-            CODIGO_PERMISO_SEGUNDO_PLANO
-        )
-    }
-
-    private fun imprimirUbicacion(ubicacion: Location){
-        binding.tvLatitud.text = "${ubicacion.latitude}"
-        binding.tvLatitud.text = "${ubicacion.longitude}"
-        Log.d("GPS", "LAT: ${ubicacion.latitude} - LON: ${ubicacion.longitude}")
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if(requestCode == CODIGO_PERMISO_SEGUNDO_PLANO){
-            val todosPermisosConcedidos = grantResults.all { it == PackageManager.PERMISSION_GRANTED }
-
-            if(grantResults.isNotEmpty() && todosPermisosConcedidos){
-                isPermisos = true
-                onPermisosConcedidos()
-            }
+    private fun requestNewLocationData(){
+        var mLocationRequest = com.google.android.gms.location.LocationRequest()
+        mLocationRequest.priority= com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,mLocationCallBack, Looper.myLooper())
         }
+
+    }
+
+    private val mLocationCallBack = object: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+            var mLastLocation : Location? = locationResult?.lastLocation ?: null
+            binding.tv1.text = "LATITUD = "+mLastLocation?.latitude.toString()
+            binding.tv2.text = "LONGITUD = "+mLastLocation?.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions():Boolean{
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        } else{
+            return false
+        }
+    }
+
+    companion object{
+        private val REQUIRED_PERMISSIONS_GPS= arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
